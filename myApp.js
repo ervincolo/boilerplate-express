641699d393734a0c0b82c51890dd86ac3b2d7ed2
Ervin Colo <79028899+ervincolo@users.noreply.github.com>
2025-06-15 01:57:47 +0200

    changes

--- myApp.js
+++ myApp.js
@@ -4,33 +4,9 @@ let app = express();
 console.log('Hello World');
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/', function(req,res){
+    res.send('Hello Express');
+});
 
 
 
