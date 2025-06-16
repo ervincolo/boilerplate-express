f94002115405cacd88f1b1eaed5cfa7169064fde
Ervin Colo <79028899+ervincolo@users.noreply.github.com>
2025-06-16 19:12:19 +0200

    somth

--- myApp.js
+++ myApp.js
@@ -13,11 +13,11 @@ app.get('/', function(req,res) {
 });
 
 app.get('/json', (req, res) => {
-    let message = "Hello json";
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-      message = message.toUpperCase();
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+      res.json({ message: 'HELLO JSON' });
+    } else {
+      res.json({ message: 'Hello json' });
     }
-    res.json({ message: message });
   });
 
 app.use('/public', express.static(__dirname + '/public'));
