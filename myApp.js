0f31975598719357f6c50a2110ddc40db812f54c
Ervin Colo <79028899+ervincolo@users.noreply.github.com>
2025-06-16 19:48:09 +0200

    changezz

--- myApp.js
+++ myApp.js
@@ -2,26 +2,22 @@ let express = require('express');
 let app = express();
 require('dotenv').config();
 
-console.log('Hello World');
-
 
 //app.get('/', function(req,res){res.send('Hello Express');});
 const absolutePath = __dirname + '/views/index.html';
+app.get('/', function(req,res) {res.sendFile(absolutePath);});
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', function(req,res) {res.sendFile(absolutePath);});
 
-
-app.use('/json', (req, res) => {
+app.get('/json', (req, res) => {
 	let response = "Hello json";
-
 	if(process.env.MESSAGE_STYLE === 'uppercase') {
 		return res.json({message:response.toUpperCase()})
 	} else {
   	return res.json({message:response})
 	}
 })
-
+console.log(process.env.MESSAGE_STYLE);
 
 
 
